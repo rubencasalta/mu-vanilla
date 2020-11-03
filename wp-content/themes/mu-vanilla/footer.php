1faf2315f545ff9ec4905700d6d31a9a5fd56cf7
Rubén Casalta <ruben.casalta@gmail.com>
2020-11-03 20:05:36 +0100

    feat: Añadir el footer

--- wp-content/themes/mu-vanilla/footer.php
+++ wp-content/themes/mu-vanilla/footer.php
@@ -1,3 +1,9 @@
+	<footer class="py-5 bg-dark text-white-50">
+		<div class="container text-center">
+			<small>Copyright © Your Website</small>
+		</div>
+	</footer>
+
 <?php wp_footer(); ?>
 </body>
 </html>
